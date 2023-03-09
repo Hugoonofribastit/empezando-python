b12d1487ff67061310a03befb70d3a0992a52eae
Hugo <hugo.onofribastit@gmail.com>
2023-03-09 15:26:15 -0300

    collections added

---
+++ COLECCIONES/listas.py
@@ -0,0 +1,9 @@
+personas = ["Mariano", "Pedro","Juan","Martin","Santiago","Daniel"]
+
+print(personas)
+
+
+mi_lista = ["Hugo",2,[1,2,3],["Mark"]]
+print(mi_lista[3])
+print(mi_lista[3][0])
+print([-1]) #va a la inversa y me devuelve el array de mark, si pongo -2 me devuelve el array de numeros
---
+++ COLECCIONES/tuplas.py
@@ -0,0 +1,11 @@
+#TUPLAS
+
+#LA DIFERENCIA CON LISTAS, ADEMAS DE LOS PARENTESIS, ES QUE NO SE PUEDEN CAMBIAR LOS ELEMENTOS
+
+mi_tupla = (1,2,3,4,5)
+mi_tupla2 = (1,2,4.5,["Hugo","Mark"],True,45)
+print(type(mi_tupla))
+print(mi_tupla)
+print(mi_tupla2)
+print(mi_tupla2[3][1])
+print(mi_tupla2[1:5])
