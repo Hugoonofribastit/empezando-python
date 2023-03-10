2ee4dafb0b7ef06f660743ffb990cc2afbcc2424
Hugo <hugo.onofribastit@gmail.com>
2023-03-10 08:11:40 -0300

    more about functions

--- FUNCIONES/declararfunciones.py
+++ FUNCIONES/declararfunciones.py
@@ -5,8 +5,17 @@ miPrimeraFuncion()
 
 i=20 #variable global
 
-def sumar():
+def variableI():
     i = 10 #variable local de la funcion
     print(i)
-sumar()
-print(i)
+variableI()
+
+print(i)#variable global
+
+
+def suma():
+    x = 10
+    y = 20
+    resultado= x + y
+    print(f"El resultado de la suma es: {resultado}")
+suma()
---
+++ FUNCIONES/retornarvalores.py
@@ -0,0 +1,3 @@
+def retornarTexto():
+    return "Texto"
+print(retornarTexto())
