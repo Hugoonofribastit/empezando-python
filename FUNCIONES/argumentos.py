e70d9c7e12304fa09260fd5945d67767c74de6a0
Hugo <hugo.onofribastit@gmail.com>
2023-03-12 11:42:29 -0300

    return

---
+++ FUNCIONES/argumentos.py
@@ -0,0 +1,11 @@
+def saludar(salu,nombre):
+    saludo = salu + nombre
+    #return saludo
+    print(f"{salu} {nombre},como estas?")
+
+#print("Hola","Hugo")
+saludar("Hola","Hugo")
+
+def suma(a,b):
+    return a+b
+print(suma(a = 10, b = 5))
--- FUNCIONES/retornarvalores.py
+++ FUNCIONES/retornarvalores.py
@@ -1,3 +1,31 @@
 def retornarTexto():
     return "Texto"
-print(retornarTexto())
+#print(retornarTexto())
+ 
+
+
+def suma(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+#print(suma(4,5))
+
+
+
+# ARGUNMENTOS INDETERMINADOS
+
+def infinit(*args): 
+    for args in args:
+        print(args)
+
+#infinit("Hola",20,10,15,[1,2,3])
+
+#indeterminados con claves
+def infinitWithKeys(**kwargs):
+    #print(kwargs)
+    for kwarg in kwargs:
+        print(kwarg,"--->",kwargs[kwarg])
+
+infinitWithKeys(a="Hola", b= 3, c=True,d =["Juan","Peter","Tincho"])
+
+ 
